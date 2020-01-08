80dfdcd6d6a5d733b76ccc80bfacfe583a3d03ab
Megan Katherine O'Brien <megankob@gmail.com>
2020-01-07 22:32:30 -0500

    Updated Comments

--- src/components/App.js
+++ src/components/App.js
@@ -11,3 +11,30 @@ function App() {
 }
 
 export default App;
+
+//Putting my TODO's here
+//TODO: Test!
+//TODO: Fill out Read.me file.
+
+//TODO: Build Components - Use the mockups as a general guide to create components.
+
+//Stateless functional components with props for:
+// - Photo Component
+// - Nav Component
+// - NotFound Component - 404 Exceeds
+
+//Stateful class components;
+// - Search
+// - Photo Container
+
+//TODO: Get a Flickr API Key - refer to directions for project. Create special data file.
+//TODO: Fetch the Flickr Data
+//TODO: Set up routes.
+//TODO: Create Search - Search for photos with the Flickr API endpoint for searching.
+//TODO: App should display at least 3 default topic links that return a list of photos matching some criteria.
+//TODO: Use axios? or fetch api
+//TODO: Display the data. Make sure to include a key so there are no warnings.
+//TODO: No more than 24 images displaying.
+
+//TODO: Exceeds if no matches are found in search, include a message to tell the user there are no matches.
+//TODO: Exceeds - Add a loading indicater when pages are loading/data is fetching.
