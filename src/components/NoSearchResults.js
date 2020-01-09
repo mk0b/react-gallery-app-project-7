3e6bce38fc4c723c7f98d11febafe670a5497418
Megan Katherine O'Brien <megankob@gmail.com>
2020-01-09 10:36:23 -0500

    Cleanup

--- src/components/NoSearchResults.js
+++ src/components/NoSearchResults.js
@@ -2,7 +2,10 @@
 import React from 'react';
 
 const NoSearchResults = () => (
-    <span>There are no results for that search, please try a different word or phrase.</span>
+    <div className="no-results">
+        <h1>No results found.</h1>
+        <p>There are no results for that search, please try a different word or phrase.</p>
+    </div>
 );
 
 export default NoSearchResults;
